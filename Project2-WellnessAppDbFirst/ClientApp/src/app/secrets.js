b465ec1105a01d8a12cd4ddd650237ddc8b1342b
jerome <jeromebrown97@gmail.com>
2021-08-10 10:29:26 -0500

    rolling back nuget packages

---
+++ Project2-WellnessAppDbFirst/ClientApp/src/app/secrets.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = void 0;
+var Secret = /** @class */ (function () {
+    function Secret() {
+    }
+    Secret.recipeKey = "207156f6a227e7d81a80d58d76ab0574";
+    return Secret;
+}());
+exports.Secret = Secret;
+//# sourceMappingURL=secrets.js.map
